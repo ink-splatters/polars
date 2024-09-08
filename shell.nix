0fab8ee33f805dbaec21e69ad9000752434357e9
Peter A. <ink.splatters@pm.me>
2024-09-08 23:39:43 +0200

    added dependencies required for `mkdocs`

--- shell.nix
+++ shell.nix
@@ -1,15 +1,24 @@
 with import <nixpkgs> { };
 let
+  getLibPath = lib: builtins.concatStringsSep ":" (map (lib: "${lib.out.outPath}/lib") lib);
   inherit (llvmPackages_18) stdenv;
   rust-jemalloc-sys' = rust-jemalloc-sys.override {
     jemalloc = jemalloc.override { disableInitExecTls = true; };
   };
+  general-libs = [
+    glib
+    pango
+    harfbuzz
+    fontconfig.lib
+  ];
 in
 mkShell.override { inherit stdenv; } rec {
   nativeBuildInputs = [
     clang_18
     llvm_18
     gnumake
+    pandoc
+    texliveMinimal
     cmake
     ninja
   ];
@@ -19,7 +28,10 @@ mkShell.override { inherit stdenv; } rec {
   ];
 
   buildInputs =
-    [ zlib-ng rust-jemalloc-sys' ]
+    [
+      zlib-ng
+      rust-jemalloc-sys'
+    ]
     ++ lib.optionals stdenv.isDarwin (
       with darwin.apple_sdk.frameworks;
       [
@@ -33,4 +45,12 @@ mkShell.override { inherit stdenv; } rec {
       ]
       ++ [ iconv ]
     );
+
+  shellHook =
+    let
+      varPrefix = if stdenv.isDarwin then "DYLD" else "LD";
+    in
+    ''
+      export ${varPrefix}_LIBRARY_PATH="${getLibPath general-libs}"
+    '';
 }
